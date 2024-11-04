591a9a6b90a9b4f8af6f3ee020a9bf085efab787
jraporta <jraporta@gmail.com>
2024-11-04 11:52:18 +0100

    Update to RestController

--- S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
+++ S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
@@ -1,19 +1,16 @@
 package cat.itacademy.s04.t01.n01.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
-    @ResponseBody
     public String greet(@RequestParam(defaultValue = "UNKNOWN") String name){
         return "Hello " + name + ". You're running a Maven project.";
     }
 
     @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
-    @ResponseBody
     public String greet2(@PathVariable(required = false) String name){
         return "Hello " + (name == null ? "UNKNOWN" : name) + ". You're running a Maven project.";
     }
--- S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
+++ S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -1,23 +1,21 @@
 package cat.itacademy.s04.t01.n02.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
 
     @GetMapping("/HelloWorld")
-    @ResponseBody
     public String greet(@RequestParam(defaultValue = "UNKNOWN") String name){
         return "Hello " + name + ". You are executing a Gradle project.";
     }
 
     @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
-    @ResponseBody
-    public String greet2(String name){
+    public String greet2(@PathVariable(required = false) String name){
         return "Hello " + (name == null ? "UNKNOWN" : name) + ". You are executing a Gradle project.";
     }
 
